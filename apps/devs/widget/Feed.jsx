eda6389889d66ba0d3527480decba40180184f9e
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2023-09-12 00:25:30 -0400

    feature: feed primitive

--- apps/devs/widget/Feed.jsx
+++ apps/devs/widget/Feed.jsx
@@ -1,8 +1,18 @@
 const index = props.index;
 const Item = props.Item;
 const Layout = props.Layout;
+const typeWhitelist = props.typeWhitelist;
+
+const renderedItems = {};
+
+Item = Item || ((props) => <div>{JSON.stringify(props)}</div>);
+Layout = Layout || (({ children }) => children);
 
 const renderItem = (item, i) => {
+  console.log(item);
+  if (typeWhitelist && !typeWhitelist.includes(item.value.type)) {
+    return false;
+  }
   return (
     <div key={JSON.stringify(item)}>
       <Item {...item} />
@@ -10,13 +20,26 @@ const renderItem = (item, i) => {
   );
 };
 
-return (
-  <Widget
-    src="devs.near/widget/PR.FilteredIndexFeed"
-    props={{
-      index,
-      renderItem,
-      Layout: ({ children }) => <Layout>{children}</Layout>,
-    }}
-  />
-);
+if (Array.isArray(index)) {
+  return (
+    <Widget
+      src="devs.near/widget/PR.MergedIndexFeed"
+      props={{
+        index,
+        renderItem,
+        Layout: ({ children }) => <Layout>{children}</Layout>,
+      }}
+    />
+  );
+} else {
+  return (
+    <Widget
+      src="devs.near/widget/PR.FilteredIndexFeed"
+      props={{
+        index,
+        renderItem,
+        Layout: ({ children }) => <Layout>{children}</Layout>,
+      }}
+    />
+  );
+}
